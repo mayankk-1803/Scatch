bb5c0510816324ae01acdf08727b8e04a864cc36
mayankk-1804 <mayankmathur1804@gmail.com>
2026-02-02 14:36:58 +0530

    routes done

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const connectDB = require('./config/mongoose-connection');
+const ownersRouter = require('./routes/ownersRouter');
+const userRouter = require('./routes/userRouter');
+const productsRouter = require('./routes/productsRouter');
+
+connectDB;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,8 +16,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.send("Hello")
-});
+app.use("/owners", ownersRouter );
+app.use("/users", userRouter );
+app.use("/products", productsRouter );
 
 app.listen(3000);
--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect()
+const connectDB = mongoose.connect("mongodb://127.0.0.1:27017/scatch")
+.then(() =>{
+    console.log("Mongoose connected successfully");
+})
+.catch((err) =>{
+    console.log("Mongoose connection failed", err);
+});
+
+module.exports = connectDB;
